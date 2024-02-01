5d0e4d915c76fd9151afcea850b363734cc93052
hristijankostadinoski <hristijan@2coders.com>
2024-02-01 22:15:06 +0100

    examples of spread operator, ternary operator and destructuring

--- cas_3/index.js
+++ cas_3/index.js
@@ -42,6 +42,76 @@ let firstNumberBiggerThenSeven = oddNumbers.find((num) => num > 7)
 // console.log(firstNumberBiggerThenSeven)
 
 // some()
-let numArray = [11, 1, 3, 17, 13, 33, 87, 10]
+let numArray = [11, 1, 3, 18, 13, 33, 87, 10]
 let numArrayHasEvenNumber = numArray.some((num) => num % 2 === 0)
 // console.log(numArrayHasEvenNumber)
+
+const isEvenNumber = (num) => {
+  return num % 2 === 0
+}
+
+// chaining high-order functions
+let randomEvenNumbers = numArray.filter(isEvenNumber).every(isEvenNumber)
+// console.log(randomEvenNumbers)
+
+// Spread operator, Ternary operator, Destructuring
+
+// spread operator
+let originalArray = [10, 20, 30, 40, 50]
+let copiedArray = [...originalArray]
+// console.log(originalArray, copiedArray)
+
+let array1 = [1, 2, 3, 4, 5]
+let array2 = [6, 7, 8, 9, 10]
+let combinedArray = array1.concat(array2) // one way
+
+let combinedArray2 = [...array1, ...array2] // second way
+//console.log(combinedArray, combinedArray2)
+
+function sumOfThreeNumbers(a, b, c) {
+  return a + b + c
+}
+
+let arrayOfThreeNumbers = [10, 20, 30]
+const result = sumOfThreeNumbers(...arrayOfThreeNumbers)
+// console.log(result)
+
+// console.log([...["a", "b", "c"], , true, {}, ...[1, 2, 3]])
+
+// ternary operator
+let checkNum = (num) => {
+  num % 2 === 0 ? console.log("Even") : console.log("Odd")
+
+  // this is the same
+  //   if (num % 2 === 0) {
+  //     console.log("Even")
+  //   } else {
+  //     console.log("Odd")
+  //   }
+}
+// checkNum(29)
+
+// destructuring
+let [
+  firstNumber,
+  secondNumber,
+  thirdNumber,
+  fourthNumber,
+  ,
+  sixthNumber,
+  ,
+  someNumber,
+] = [1, 2, 3, 10, 9, 82]
+console.log(someNumber)
+
+const randomPerson = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 23,
+}
+
+let name = randomPerson.firstName // one way
+console.log(name)
+
+const { firstName: name2, age } = randomPerson
+console.log(name2, age)
