85076b91301140f19d7dedd3c8846fe9717a1a51
hristijankostadinoski <hristijan@2coders.com>
2024-02-15 21:36:26 +0100

    example of POST method

--- cas_7/index.js
+++ cas_7/index.js
@@ -18,7 +18,7 @@ const parsedData = JSON.parse(jsonString)
 
 // FETCH Method
 function fetchUsers() {
-  fetch("https://jsonplaceholder.typicode.com/userss")
+  fetch("https://jsonplaceholder.typicode.com/users")
     .then((response) => response.json())
     .then((data) => {
       const emailContainer = document.getElementById("email-container")
@@ -32,4 +32,46 @@ function fetchUsers() {
     .catch((error) => console.log(error))
 }
 
-fetchUsers()
+// fetchUsers()
+
+// GET Method using async/await, try/catch
+const fetchUsersData = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users")
+    const data = await response.json()
+
+    const emailContainer = document.getElementById("email-container")
+
+    data.forEach((user) => {
+      const emailParagraph = document.createElement("p")
+      emailParagraph.textContent = user.email
+      emailContainer.appendChild(emailParagraph)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// fetchUsersData()
+
+// POST Method
+const postData = () => {
+  const newPost = {
+    userId: 11,
+    title: "Post 11",
+    body: "Random Post Body",
+  }
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(newPost),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
+}
+
+postData()
